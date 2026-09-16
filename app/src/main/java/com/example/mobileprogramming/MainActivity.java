e6d8344eede9b5aefd4a3289fa2076f8ca89a4ee
Santosh Neupane <sanneupane1024@gmail.com>
2026-09-16 18:17:30 +0545

    Enable edge-to-edge display and handle window insets

--- app/src/main/java/com/example/mobileprogramming/MainActivity.java
+++ app/src/main/java/com/example/mobileprogramming/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.mobileprogramming;
 
 import android.os.Bundle;
 
+import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //- enable edge-to-edge display
+        //- could be excluded, for exam
+        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
+
+        //- used to handle window insets in an Android app
+        //- could be excluded for exam
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //- Insets represent the areas of the screen occupied by system UI
+        //- elements like the status bar, navigation bar, and gesture insets.
     }
 }
